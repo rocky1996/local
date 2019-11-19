d5e97471be3750596f5fad6da8fb28f4434cbb7e
wujinfan <wujinfan@tal.com>
2019-11-19 20:10:04 +0800

    local项目新增玩耍状态指令和缓存指令

---
+++ src/main/java/com/acat/common/CacheConst.java
@@ -0,0 +1,15 @@
+package com.acat.common;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: wujinfan
+ * Date: 2019-11-19
+ */
+public class CacheConst {
+
+    /**
+     * 记录孩子玩耍的状态
+     */
+    public static final String childPlayState = "acat:childPlaySate:{0}:{1}";
+
+}
---
+++ src/main/java/com/acat/common/CommandConst.java
@@ -0,0 +1,19 @@
+package com.acat.common;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: wujinfan
+ * Date: 2019-11-19
+ */
+public class CommandConst {
+
+    //正常玩耍状态
+    public static final String NORMAL = "NORMAL";
+
+    //开始发苹果状态
+    public static final String START_SEND_APPLE = "START_SEND_APPLE";
+
+    //停止发水果状态
+    public static final String STOP_SEND_APPLE = "STOP_SEND_APPLE";
+
+}
